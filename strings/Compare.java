304209b69326b313a1c154a9239f6a419df1ee3b
pranalimagdum-12 <paranlimagdum1210@gmail.com>
2026-01-23 19:56:27 +0530

    Comparing two strings

---
+++ strings/Compare.java
@@ -0,0 +1,20 @@
+public class Compare{
+    public static void main(String args[]){
+        //Comapring two strings
+        String name1= "Tony";
+        String name2 = "Tony";
+        //s1 > s2 : +ve value
+        //s1<s2 : -ve value
+        //s1 = s2 : Zero
+
+        if(name1.compareTo(name2) == 0){
+            System.out.println("Srings are equal");
+        }
+        else{
+            System.out.println("Strings are not equal");
+        
+    }
+    }
+}
+
+
