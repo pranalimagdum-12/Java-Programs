6dc7c87b01949844638fdebe7a18199026745190
pranalimagdum-12 <paranlimagdum1210@gmail.com>
2026-01-25 17:56:46 +0530

    Extract username from an email

---
+++ strings/ExtractUsername.java
@@ -0,0 +1,21 @@
+//Input an email from the user. You have to create a username from the email by deleting the part that comes after ‘@’. Display that username to the user.
+import java.util.Scanner;
+public class ExtractUsername {
+   public static void main(String args[]) {
+     Scanner sc = new Scanner (System.in);
+     String email = sc.next();
+     String userName = "";
+
+
+     for(int i=0; i<email.length(); i++) {
+       if(email.charAt(i) == '@') {
+        break;
+       } else {
+         userName += email.charAt(i);
+       }
+     }
+
+
+     System.out.println(userName);
+   }
+}
