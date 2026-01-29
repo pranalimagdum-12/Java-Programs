5a5d9cbbdf9343659fafcf995d0b7f129956c6d2
pranalimagdum-12 <paranlimagdum1210@gmail.com>
2026-01-29 22:48:47 +0530

    Sorting Array using Bubble Sort

---
+++ Sorting/Bubblesort.java
@@ -0,0 +1,25 @@
+public class Bubblesort{
+    public static void printArray(int arr[]){
+        for(int i =0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+    public static void main(String args[]){
+        int arr [] = {7,8,3,1,2};
+
+        //Bubble sort
+        for(int i = 0;i<arr.length;i++){
+            for(int j=0 ;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    //Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        printArray(arr);
+
+    }
+}
