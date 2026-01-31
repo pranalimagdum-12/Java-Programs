d166e194956aded41336aca993798062cacedf21
pranalimagdum-12 <paranlimagdum1210@gmail.com>
2026-01-31 17:46:50 +0530

    Recursive sum calculation

---
+++ Recursion/Sum.java
@@ -0,0 +1,25 @@
+public class Sum {
+    public static void PrintSum(int i, int n, int sum){
+        if (i == n){
+            sum += i;
+            System.out.println(sum);
+            return;
+        }
+            sum += i;
+            PrintSum(i+1, n, sum);
+            System.out.println(i);
+        }
+        public static void main(String args[]){
+            PrintSum(1, 5, 0);
+
+
+        }
+}
+
+
+           
+        
+
+                       
+       
+
