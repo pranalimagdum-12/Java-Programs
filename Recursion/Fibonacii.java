c0b585bf732b0bca0aa9f16c9f1124b9296d82d0
pranalimagdum-12 <paranlimagdum1210@gmail.com>
2026-02-01 13:28:46 +0530

    Recursive Fibonacii Print

---
+++ Recursion/Fibonacii.java
@@ -0,0 +1,23 @@
+public class Fibonacii{
+    public static void Printfib(int a, int b, int n){
+        if(n ==0){
+             return;
+        }
+        int c = a + b;
+        System.out.println(c);
+        Printfib(b, c, n-1);
+        
+
+        }
+        public static void main(String args[]){
+            int n =7;
+            int a =0, b=1;
+            System.out.println(a);
+            System.out.println(b);
+            Printfib(a, b, n-2);
+        }
+
+
+        
+    }
+
